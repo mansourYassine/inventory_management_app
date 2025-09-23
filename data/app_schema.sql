e8d718322d1fc693833d0a303484e4bae8fbb154
yassine mansour <yassineaitman@gmail.com>
2025-09-24 00:04:35 +0100

    update queries

--- data/app_schema.sql
+++ data/app_schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE `suppliers` (
     `supplier_id` INT NOT NULL AUTO_INCREMENT,
     `supplier_name` VARCHAR(50) UNIQUE NOT NULL,
     `supplier_email` VARCHAR(50) UNIQUE NOT NULL,
-    `supplier_phone` VARCHAR(20) UNIQUE,
-    `supplier_adress` VARCHAR(255) UNIQUE NOT NULL,
+    `supplier_phone` VARCHAR(20),
+    `supplier_address` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`supplier_id`)
 );
 CREATE TABLE `categories` (
@@ -19,7 +19,7 @@ CREATE TABLE `products` (
     `category_id` INT,
     `supplier_id` INT NOT NULL,
     `product_quantity` INT NOT NULL,
-    `product_price` DOUBLE NOT NULL,
+    `product_price` DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (`product_id`),
     FOREIGN KEY (`supplier_id`) REFERENCES `suppliers`(`supplier_id`),
     FOREIGN KEY (`category_id`) REFERENCES `categories`(`category_id`)
@@ -28,7 +28,7 @@ CREATE TABLE `stock_movements` (
     `stock_movement_id` INT NOT NULL AUTO_INCREMENT,
     `product_id` INT NOT NULL,
     `movement_quantity` INT NOT NULL,
-    `movement_type` VARCHAR(50) NOT NULL,
+    `movement_type` ENUM('IN','OUT') NOT NULL,
     `movement_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     PRIMARY KEY (`stock_movement_id`),
     FOREIGN KEY (`product_id`) REFERENCES `products`(`product_id`)
