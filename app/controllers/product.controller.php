90c0e1a147846c8f02249fd54360d0e1fa5f0f82
yassine mansour <yassineaitman@gmail.com>
2025-10-09 18:09:02 +0100

    put $uri['path'] in a variable to use it

--- app/controllers/product.controller.php
+++ app/controllers/product.controller.php
@@ -3,18 +3,19 @@ declare(strict_types=1);
 require DB_CONNECTION_PATH . 'db.php';
 require MODELS_PATH . 'product.model.php';
 
-if (strcmp($uri['path'], '/products') === 0) {
+$path = $uri['path'];
+if (strcmp($path, '/products') === 0) {
     $products = getAllProducts();
     mysqli_close($connect);
     require VIEWS_PATH . 'product/products.view.php';
 } else {
-    if (strcmp($uri['path'], '/products/info') === 0) {
+    if (strcmp($path, '/products/info') === 0) {
         $productId = intval($_GET['product_id']);
         $productInfo = getProductInfo($productId);
         require VIEWS_PATH . 'product/product_info.view.php';
-    } elseif (strcmp($uri['path'], '/products/add') === 0) {
+    } elseif (strcmp($path, '/products/add') === 0) {
         require VIEWS_PATH . 'product/add_product.view.php';
-    } elseif (strcmp($uri['path'], '/products/edit') === 0) {
+    } elseif (strcmp($path, '/products/edit') === 0) {
         require VIEWS_PATH . 'product/edit_product.view.php';
     } else {
         abort();
