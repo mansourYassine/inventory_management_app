8a3e9c3a2c20a323e6e14aabaa4e2a6bcf367e4e
yassine mansour <yassineaitman@gmail.com>
2025-10-15 07:12:15 +0100

    make the dashboard view

--- app/controllers/dashboard.controller.php
+++ app/controllers/dashboard.controller.php
@@ -3,12 +3,9 @@ declare(strict_types=1);
 require DB_CONNECTION_PATH . 'db.php';
 require MODELS_PATH . 'dashboard.model.php';
 
-// $totalProductsNumber = getTotalProdutsNumber($connect);
-// $totalProductsQuantity = getTotalProdutsQuantity($connect);
-// $totalProductsValue = getTotalProdutsValue($connect);
-
-// $lowStockProducts = getLowStockProducts($connect);
-
-// dd($lowStockProducts);
+$totalProductsNumber = getTotalProdutsNumber($connect);
+$totalProductsQuantity = getTotalProdutsQuantity($connect);
+$totalProductsValue = getTotalProdutsValue($connect);
+$lowStockProducts = getLowStockProducts($connect);
 
 require (VIEWS_PATH . 'dashboard.view.php');
--- app/views/dashboard.view.php
+++ app/views/dashboard.view.php
@@ -9,5 +9,22 @@
         <?php require VIEWS_PATH . 'includes\header.php' ?>
         <?php require VIEWS_PATH . 'includes\navbar.php' ?>
         <h1>Dashboard Page</h1>
+        <ul>
+            <li>Total number of products: <?= $totalProductsNumber ?></li>
+            <li>Total quantity : <?= $totalProductsQuantity ?></li>
+            <li>Total value : <?= $totalProductsValue ?></li>
+        </ul>
+        <table border="1">
+            <tr>
+                <th>Product Name</th>
+                <th>Product Quantity</th>
+            </tr>
+            <?php foreach ($lowStockProducts as $product) :?>
+                <tr>
+                    <td><?= $product['product_name'] ?></td>
+                    <td><?= $product['product_quantity'] ?></td>
+                </tr>
+            <?php endforeach ?>
+        </table>
     </body>
 </html>
